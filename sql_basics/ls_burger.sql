73bdcdc98d23f966d06423dbd19d820256513e6a
vando <vandorramos@gmail.com>
2023-07-05 09:58:09 -0300

    Create and views databases.

--- sql_basics/ls_burger.sql
+++ sql_basics/ls_burger.sql
@@ -26,14 +26,18 @@ INSERT INTO orders VALUES (5, 'Joyce Silva', 'LS Chicken Burger', 'Onion Rings',
 
 -- Exercises
 
--- Write a query that returns all of the customer names from the orders table.
--- Solution: SELECT customer_name FROM orders;
+-- 1. Write a query that returns all of the customer names from the orders table.
+-- Solution: 
+-- SELECT customer_name FROM orders;
 
--- Write a query that returns all of the orders that include a Chocolate Shake.
--- Solution: SELECT * FROM orders WHERE drink = 'Chocolate Shake';
+-- 2. Write a query that returns all of the orders that include a Chocolate Shake.
+-- Solution: 
+-- SELECT * FROM orders WHERE drink = 'Chocolate Shake';
 
--- Write a query that returns the burger, side, and drink for the order with an id of 2.
--- Solution: SELECT burger, side, drink FROM orders WHERE id = 2;
+-- 3. Write a query that returns the burger, side, and drink for the order with an id of 2.
+-- Solution: 
+-- SELECT burger, side, drink FROM orders WHERE id = 2;
 
--- Write a query that returns the name of anyone who ordered Onion Rings.
--- Solution: SELECT customer_name FROM orders WHERE side = 'Onion Rings';
+-- 4. Write a query that returns the name of anyone who ordered Onion Rings.
+-- Solution: 
+-- SELECT customer_name FROM orders WHERE side = 'Onion Rings';
