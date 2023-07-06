4b546a3f7095ad1bca6e5978aa9f3d83245d9384
vando <vandorramos@gmail.com>
2023-07-06 10:46:13 -0300

    Alter table syntax, renaming a table and a column, changing a columns data type, adding and removing constraint, adding and removing columns, droping a table.

--- 1_sql_basics/ls_burger.sql
+++ 1_sql_basics/ls_burger.sql
@@ -27,17 +27,25 @@ INSERT INTO orders VALUES (5, 'Joyce Silva', 'LS Chicken Burger', 'Onion Rings',
 -- Exercises
 
 -- 1. Write a query that returns all of the customer names from the orders table.
--- Solution: 
--- SELECT customer_name FROM orders;
+
+-- Solution:
+
+SELECT customer_name FROM orders;
 
 -- 2. Write a query that returns all of the orders that include a Chocolate Shake.
--- Solution: 
--- SELECT * FROM orders WHERE drink = 'Chocolate Shake';
+
+-- Solution:
+
+SELECT * FROM orders WHERE drink = 'Chocolate Shake';
 
 -- 3. Write a query that returns the burger, side, and drink for the order with an id of 2.
--- Solution: 
--- SELECT burger, side, drink FROM orders WHERE id = 2;
+
+-- Solution:
+
+SELECT burger, side, drink FROM orders WHERE id = 2;
 
 -- 4. Write a query that returns the name of anyone who ordered Onion Rings.
--- Solution: 
--- SELECT customer_name FROM orders WHERE side = 'Onion Rings';
+
+-- Solution:
+
+SELECT customer_name FROM orders WHERE side = 'Onion Rings';
--- 2_create_and_views_databases/exercises.sql
+++ 2_create_and_views_databases/exercises.sql
@@ -1,29 +1,45 @@
 -- 1. From the Terminal, create a database called database_one.
--- Solution: 
+
+-- Solution:
+
 createdb database_one
 
 -- 2. From the Terminal, connect via the psql console to the database that you created in the previous question.
--- Solution: 
+
+-- Solution:
+
 psql -d database_one
 
 -- 3. From the psql console, create a database called database_two.
--- Solution: 
+
+-- Solution:
+
 CREATE DATABASE database_two;
 
 -- 4. From the psql console, connect to database_two.
+
 -- Solution:
+
 \c database_two
 
 -- 5. Display all of the databases that currently exist.
+
 -- Solution:
+
 \list
 
 -- 6. From the psql console, delete database_two.
+
 -- Solution:
+
 \c database_one
+
 DROP DATABASE database_two;
 
 -- 7. From the Terminal, delete the database_one and ls_burger databases.
+
 -- Solution:
+
 dropdb database_one
+
 dropdb ls_burger
